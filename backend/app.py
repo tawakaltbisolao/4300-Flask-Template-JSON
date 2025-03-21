7e5fa3073f4f33d57ad1c00332ff4e7dd5d6fb33
Jangwoo Lee <jangwoo24@gmail.com>
2025-03-21 03:26:48 -0400

    Compute cosine sim
    Only for shampoo so far. Also need to change frontend to parse product names rather than print raw output

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,16 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+    # shampoo_df = data.iloc[:35501]
+    # conditioner_df = data.iloc[35501:67892]
+    # oil_df = data.iloc[67892:]
+
+cond_min_index = 35501
+oil_min_index = 67892
 
 # ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
@@ -18,13 +28,23 @@ json_file_path = os.path.join(current_directory, 'init.json')
 # Assuming your JSON data is stored in a file named 'init.json'
 with open(json_file_path, 'r') as file:
     data = json.load(file) #list of dictionaries
-    #each datapoint has "product_name", "star", "content", and "profile" (might be blank)
-    shampoo_df = pd.DataFrame(data[:35501])
-    conditioner_df = pd.DataFrame(data[35501:67892])
-    oil_df = pd.DataFrame(data[67892:])
-
     # episodes_df = pd.DataFrame(data['episodes'])
     # reviews_df = pd.DataFrame(data['reviews'])
+
+#each datapoint has "product_name", "star", "content", and "profile" (might be blank)
+data = pd.DataFrame(data) #pandas DataFrame
+data['content'] = data['content'].map(lambda x: x.strip())
+data['content'] = data['profile'] + ' ' + data['content'] #PREpend profile info (even if blank)
+# data['content'] = data['content'].map(lambda x: x.strip()) #could strip again but IDC
+data = data.drop('profile', axis=1) #no longer needed in the demo
+
+vec = TfidfVectorizer(max_df=0.8, min_df=10)
+tdidf_matrix = vec.fit_transform(data['content']).toarray()
+
+
+    # shampoo_df = data.iloc[:35501]
+    # conditioner_df = data.iloc[35501:67892]
+    # oil_df = data.iloc[67892:]
 
 app = Flask(__name__)
 CORS(app)
@@ -38,9 +58,24 @@ CORS(app)
 #     matches_filtered_json = matches_filtered.to_json(orient='records')
 #     return matches_filtered_json
 
-def reviews_json_search():
-    #TODO: man
-    return
+def reviews_json_search(wantPoo, wantCond, wantOil, query):
+    #Thanks Bisola!
+    query_vec = vec.transform([query]).toarray().T
+    poo_matches = []
+    cond_matches = []
+    oil_matches = []
+    if wantPoo:
+        poo_docs = tdidf_matrix[:cond_min_index]
+        poo_docs = np.dot(poo_docs, query_vec).flatten() #shoud have len(poo_docs)
+        poo_docs = np.argmax(poo_docs) #now just a single index
+        #TODO AFTER DEMO: change this to top 5 or top 10 reviews per product, not just 1
+        # also means change HTML to expect LIST instead of single
+        poo_matches.append(data.iloc[poo_docs].to_json(orient='records'))
+        print(poo_matches)
+    #TODO: conditioner and oil
+    
+
+    return poo_matches
 
 
 @app.route("/")
@@ -50,8 +85,11 @@ def home():
 
 @app.route("/reviews")
 def reviews_search():
-    text = request.args.get("query")
-    return json_search(text)
+    query = request.args.get("query")
+    wantPoo = request.args.get("wantPoo")
+    wantCond = request.args.get("wantCond")
+    wantOil = request.args.get("wantOil")
+    return reviews_json_search(wantPoo, wantCond, wantOil, query)
     # return reviews_json_search(text)
 
 # @app.route("/episodes")
