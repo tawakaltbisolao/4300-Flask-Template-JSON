701b22e59653680c0f5976ca596c2a4342a3130f
Jangwoo Lee <jangwoo.c.24@gmail.com>
2025-04-25 16:50:12 -0400

    Fix critical error in vectorizer

--- backend/app.py
+++ backend/app.py
@@ -32,6 +32,7 @@ json_file_path = os.path.join(current_directory, 'dataset', 'ulta_reviews.json')
 vocab_file_path = os.path.join(current_directory, 'dataset', 'ulta_vocabulary.json')
 index_to_id_file_path = os.path.join(current_directory, 'dataset', 'ulta_product_ids.json')
 products_file_path = os.path.join(current_directory, 'dataset', 'products_ulta.json')
+allergies_file_path = os.path.join(current_directory, 'dataset', 'allergy_map.json')
 
 
 # Assuming your JSON data is stored in a file named 'init.json'
@@ -46,8 +47,8 @@ with open(json_file_path, 'r') as file:
 with open(vocab_file_path, 'r') as f:
     vocab = json.load(f) # ngram to index dictionary
 # vec = TfidfVectorizer(max_df=0.8, min_df=10, ngram_range=(1,3))
-vec = TfidfVectorizer(vocabulary=vocab)
-vocab = None
+vec = TfidfVectorizer(vocabulary=vocab, ngram_range=(1,2))
+# vocab = None
 vec.fit(data)
 # data = None
 # with open(json_file_path, 'r') as file:
@@ -91,7 +92,7 @@ CORS(app)
 #     matches_filtered_json = matches_filtered.to_json(orient='records')
 #     return matches_filtered_json
 
-def reviews_json_search(wantPoo, wantCond, wantOil, query):
+def reviews_json_search(wantPoo, wantCond, wantOil, query, bad_idx):
     #Thanks Bisola!
     # print("wantPoo: " + wantPoo)
     # print("wantCond: " + wantCond)
@@ -106,14 +107,20 @@ def reviews_json_search(wantPoo, wantCond, wantOil, query):
         products_info = json.load(f)
 
     query_vec = vec.transform([query]).toarray() 
+    print(query_vec)
     query_vec = normalize(np.dot(query_vec, words_compressed_normalized)).squeeze()
     poo_matches = '[]'
     cond_matches = '[]'
     oil_matches = '[]' #if actual arrays, JS gets mad when parsing
     if wantPoo:
+        bad_idx_poo = [x for x in bad_idx if x<cond_min_index]
         poo_docs = docs_compressed_normalized[:cond_min_index]
+        poo_docs = np.delete(poo_docs, bad_idx_poo, axis=0)
+
         poo_docs = np.dot(poo_docs, query_vec)
+        print(poo_docs[0:5])
         poo_docs = np.argsort(-poo_docs)[:5] #sort descending, then get first 5
+        print(poo_docs)
         # poo_matches = data.iloc[poo_docs].to_json(orient='records')
         # poo_docs = [data[i] for i in poo_docs]
         poo_docs = [index_to_id[i] for i in poo_docs]
@@ -129,7 +136,9 @@ def reviews_json_search(wantPoo, wantCond, wantOil, query):
         # poo_matches = data.iloc[poo_docs].to_json(orient='records')
 
     if wantCond:
+        bad_idx_cond = [x-cond_min_index for x in bad_idx if x<oil_min_index and x>=cond_min_index]
         cond_docs = docs_compressed_normalized[cond_min_index:oil_min_index]
+        cond_docs = np.delete(cond_docs, bad_idx_cond, axis=0)
         cond_docs = np.dot(cond_docs, query_vec)
         cond_docs = np.argsort(-cond_docs)[:5] #sort descending, then get first 5
         # cond_matches = data.iloc[cond_docs].to_json(orient='records')
@@ -144,7 +153,9 @@ def reviews_json_search(wantPoo, wantCond, wantOil, query):
         # cond_docs += cond_min_index
         # cond_matches = data.iloc[cond_docs].to_json(orient='records')
     if wantOil:
+        bad_idx_oil = [x-oil_min_index for x in bad_idx if x>=oil_min_index]
         oil_docs = docs_compressed_normalized[oil_min_index:]
+        oil_docs = np.delete(oil_docs, bad_idx_oil, axis=0)
         oil_docs = np.dot(oil_docs, query_vec)
         oil_docs = np.argsort(-oil_docs)[:5] #sort descending, then get first 5
         # oil_matches = data.iloc[oil_docs].to_json(orient='records')
@@ -162,11 +173,11 @@ def reviews_json_search(wantPoo, wantCond, wantOil, query):
 
     #return should be dict or list of dicts;
     #JS will interpret at actual JSON
-    print('shampoo matches', poo_matches)
-    print()
-    print('conditioner matches', cond_matches)
-    print()
-    print('oil matches', oil_matches)
+    # print('shampoo matches', poo_matches)
+    # print()
+    # print('conditioner matches', cond_matches)
+    # print()
+    # print('oil matches', oil_matches)
     return {'shampoos': poo_matches, 'conditioners': cond_matches, 'oils': oil_matches}
 
 
@@ -181,7 +192,27 @@ def reviews_search():
     wantPoo = request.args.get("wantPoo") == 'true'
     wantCond = request.args.get("wantCond") == 'true'
     wantOil = request.args.get("wantOil") == 'true'
-    return reviews_json_search(wantPoo, wantCond, wantOil, query)
+
+    bad_idx = set()
+    with open(allergies_file_path, 'r') as f:
+        allergies = json.load(f)
+    if(request.args.get('no_sulfate') == 'true'):
+        bad_idx.update(allergies['sulfates'])
+    if(request.args.get('no_paraben') == 'true'):
+        bad_idx.update(allergies['parabens'])
+    if(request.args.get('no_fragrance') == 'true'):
+        bad_idx.update(allergies['fragrances'])
+    if(request.args.get('no_seed') == 'true'):
+        bad_idx.update(allergies['seeds'])
+    if(request.args.get('no_nut') == 'true'):
+        bad_idx.update(allergies['nuts'])
+    if(request.args.get('no_alcohol') == 'true'):
+        bad_idx.update(allergies['alcohols'])
+    
+    bad_idx = sorted(list(bad_idx))
+
+
+    return reviews_json_search(wantPoo, wantCond, wantOil, query, bad_idx)
     # return reviews_json_search(text)
 
 # @app.route("/episodes")
